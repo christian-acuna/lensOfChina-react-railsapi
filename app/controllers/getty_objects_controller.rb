4e4dbf80925482007f3092197c36707ee4cd3b1c
Christian Acuña <cacuna0828@gmail.com>
2016-11-29 09:52:57 -0800

    create rails api

--- app/controllers/getty_objects_controller.rb
+++ app/controllers/getty_objects_controller.rb
@@ -1,4 +1,8 @@
 class GettyObjectsController < ApplicationController
   def index
+    render(
+      status: 200,
+      json: GettyObject.all
+    )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'getty_objects/index'
+  scope '/api/getty' do
+    get :records, to: 'getty_objects#index'
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
