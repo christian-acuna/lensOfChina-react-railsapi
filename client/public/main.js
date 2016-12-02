ed839bd7587b9ae8bfeef3599e454ce8df354bed
Christian Acuña <cacuna0828@gmail.com>
2016-12-01 16:56:35 -0800

    add map that moves to side

---
+++ client/public/main.js
@@ -0,0 +1,11 @@
+$(function() {
+  // $('.ui.sticky')
+  //   .sticky({
+  //     context: '#root'
+  //   });
+
+  $('.overlay').visibility({
+    type: 'fixed',
+    offset: 80
+  });
+});
---
+++ client/public/map.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiY2hyaXN0aWFubWluZ2xlcyIsImEiOiIzMjdlOTI2ODMwNDg4MWI1ZjdhZjE0MGE2ZjQ5NjQ5NiJ9.OAso8BqXVyQeMCxaX_6FMA';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v9',
+    zoom: 7,
+    center: [120.9162999, 31.2231338]
+});
