537331480cbee35bbff16ef45dcb8eb880379d76
Christian Acuña <cacuna0828@gmail.com>
2016-12-01 17:37:16 -0800

    config map

--- client/public/map.js
+++ client/public/map.js
@@ -2,6 +2,31 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiY2hyaXN0aWFubWluZ2xlcyIsImEiOiIzMjdlOTI2ODMwN
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v9',
-    zoom: 7,
-    center: [120.9162999, 31.2231338]
+    zoom: 10,
+    pitch: 25,
+    // bearing: -15.6,
+    center: [121.487614, 31.2217468]
+});
+
+map.on('load', function() {
+    map.addLayer({
+        'id': '3d-buildings',
+        'source': 'composite',
+        'source-layer': 'building',
+        'filter': ['==', 'extrude', 'true'],
+        'type': 'fill-extrusion',
+        'minzoom': 7,
+        'paint': {
+            'fill-extrusion-color': '#aaa',
+            'fill-extrusion-height': {
+                'type': 'identity',
+                'property': 'height'
+            },
+            'fill-extrusion-base': {
+                'type': 'identity',
+                'property': 'min_height'
+            },
+            'fill-extrusion-opacity': .6
+        }
+    });
 });
