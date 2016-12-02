1c47f2336977cd1d5d2c2d40273fc6a3c427807b
Christian Acuña <cacuna0828@gmail.com>
2016-12-01 18:40:19 -0800

    update cap file

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :port,            22
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 set :rbenv_type, :user # or :system, depends on your rbenv setup
-set :rbenv_ruby, 'ruby 2.2.5'
+set :rbenv_ruby, '2.2.5'
 
 # Don't change these unless you know what you're doing
 set :pty,             true
